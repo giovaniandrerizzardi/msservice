92c39bdbe3ffdfcbf9fa14395ca80967c6c48b4b
Giovani Rizzardi PC <giovaniandrerizzardi@hotmail.com>
2019-07-26 20:27:48 -0300

    sadasd

--- testeFile.py
+++ testeFile.py
@@ -7,6 +7,6 @@ dados = decoder.processData_decode("EV_275")
 
 
 #interscityManager.getDataDaily("9c0772b8-c809-4865-bec7-70dd2013bc37")
-interscityManager.getDataByRange("9c0772b8-c809-4865-bec7-70dd2013bc37","2018-07-14T14:56:20","2019-07-15T19:56:20")
+#interscityManager.getDataByRange("9c0772b8-c809-4865-bec7-70dd2013bc37","2018-07-14T14:56:20","2019-07-15T19:56:20")
 #a = json.dumps(x)
 #print(y)
