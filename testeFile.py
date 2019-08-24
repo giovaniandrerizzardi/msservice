1fc1748807126508e1fe3c03e361a8925af7f964
Giovani Rizzardi PC <giovaniandrerizzardi@hotmail.com>
2019-08-24 15:22:28 -0300

    flow e envio dos dados pro glow

--- testeFile.py
+++ testeFile.py
@@ -1,7 +1,16 @@
 from processors import decoder, interscityManager
 import json
- 
+import requests
 dados = decoder.processData_decode("EV_275")
+
+datajson = {
+        "event_type": dados.Event,
+        "energy_ativa": dados.energy_ativa,
+        "voltage_real_rms": dados.rmsVoltage_real,
+        "phase_real_rms": dados.rmsPhase_real
+    }
+
+requests.post("http://127.0.0.1:1880/attstatus", data=datajson)
 
 #interscityManager.sendInfoToInterSCity(dados)
 
@@ -10,3 +19,4 @@ dados = decoder.processData_decode("EV_275")
 #interscityManager.getDataByRange("9c0772b8-c809-4865-bec7-70dd2013bc37","2018-07-14T14:56:20","2019-07-15T19:56:20")
 #a = json.dumps(x)
 #print(y)
+
