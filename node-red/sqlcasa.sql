60ee6150ca01245a9a4d9f0acaab49f39fca9039
Giovani Rizzardi PC <giovaniandrerizzardi@hotmail.com>
2019-11-15 22:11:39 -0300

    cd

---
+++ node-red/sqlcasa.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT * FROM mestrado.casa_info
+LIMIT 0, 1000
+
+-- Date: 2019-11-15 21:23
+*/
+INSERT INTO `` (`id`,`uuid`,`nr_residentes`,`corrente_nominal`,`public_building`,`tensao_nominal`,`latitude`,`longitude`,`cidade`,`senha`) VALUES (1,'407da65d-712a-4a8a-b3ca-6eb8e8881374',3,0.3,0,220,'-28.27278','-52.40667',1,'123456789');
+INSERT INTO `` (`id`,`uuid`,`nr_residentes`,`corrente_nominal`,`public_building`,`tensao_nominal`,`latitude`,`longitude`,`cidade`,`senha`) VALUES (2,'d750d04e-b64f-4a56-9b02-6437f795cd84',1,0.3,1,220,'-28.27278','-52.416669999999996',1,'123456789');
+INSERT INTO `` (`id`,`uuid`,`nr_residentes`,`corrente_nominal`,`public_building`,`tensao_nominal`,`latitude`,`longitude`,`cidade`,`senha`) VALUES (3,'8a571135-9010-4f56-b930-59587de8167a',6,0.3,1,220,'-28.27278','-52.416669999999996',1,'123456789');
+INSERT INTO `` (`id`,`uuid`,`nr_residentes`,`corrente_nominal`,`public_building`,`tensao_nominal`,`latitude`,`longitude`,`cidade`,`senha`) VALUES (4,'c62824b8-8500-415a-87c8-b4b4906422e5',8,0.3,1,220,'-28.27278','-52.416669999999996',1,'123456789');
+INSERT INTO `` (`id`,`uuid`,`nr_residentes`,`corrente_nominal`,`public_building`,`tensao_nominal`,`latitude`,`longitude`,`cidade`,`senha`) VALUES (5,'cf7ab1dd-dfc2-400b-b772-d3c3fa4140da',6,0.3,1,220,'-28.29128','-52.430499999999995',1,'123456789');
